cf431c1deb2f6131baad47549e87536f45f7028d
Karan Kumar Mishra <93134411+Karan-Kumar-Mishra@users.noreply.github.com>
2022-10-02 16:29:21 +0530

    Add files via upload

---
+++ operator_overloading1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+class temp
+{
+    int a,b;
+public:
+    void display()
+    {
+        cout<<"a="<<a<<endl;
+        cout<<"a="<<a<<endl;
+    }
+    void input()
+    {
+        cin>>a;
+        cin>>b;
+    }
+    friend temp operator+(temp &t1,temp &t2);
+};
+temp operator+(temp &t1,temp &t2)
+{
+    temp t3;
+    t3.a=t1.a+t2.a;
+    t3.b=t1.b+t2.b;
+    return t3;
+}
+int main()
+{
+    temp n1,n2,n3;
+    n1.input();
+    n2.input();
+    n3=n1+n2;
+    n3.display();
+    return 0;
+}
